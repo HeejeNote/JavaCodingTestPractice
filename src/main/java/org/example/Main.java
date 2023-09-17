7134b7f90b6860797f4e7a00f36f525d2d06c40c
Heeje <tjdgmlwp34567@gmail.com>
2023-09-17 19:10:44 +0900

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("입력하시오");
+        int input1 = sc.nextInt();
+//        int input2 = sc.nextInt();
+        System.out.println("input = " + input1);
+        return ;
+    }
+}
