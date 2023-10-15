071b4f0efa21529b5fbff96dd4e15b3d056af2a9
Heeje <tjdgmlwp34567@gmail.com>
2023-10-15 23:47:05 +0900

    [String] 반복문자 개수 표시

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,28 @@
-package org.example;public class Main {
+package org.example;
+
+
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "teachermode e";
+
+        String[] split = str.split(" ");
+        char[] wordArr = split[0].toCharArray();
+        char c = split[1].charAt(0);
+        int distance = 0;
+        int[] distanceArr = new int[split[0].length()];
+        // t 0 1
+        for(int i=0; i < split[0].length(); i++){
+            while(wordArr[i] != c){
+                if(wordArr[i] == c){
+                    distance = i;
+                    break;
+                }
+                distance++;
+            }
+            distanceArr[i] = distance;
+            System.out.println("distanceArr = " + distanceArr[i]);
+        }
+
     }
 }
---
+++ src/main/java/org/example/stringEx/RepeatString.java
@@ -0,0 +1,47 @@
+package org.example.stringEx;
+
+import java.util.Scanner;
+
+public class RepeatString {
+    /**
+     * Q. 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는 문자 바로 오른쪽에 반복 횟수를 표기(1인 경우 생략)
+     * 입력 예시 :::
+     * KKHSSSSSSSE
+     * 출력 예시 :::
+     * K2HS7E
+     */
+
+    public String solution(String str){
+        String answer = "";
+        str = str + " ";
+        int cnt = 1;
+
+        for(int i = 0; i < str.length() -1; i++){
+            if(str.charAt(i) == str.charAt(i+1)){
+                cnt ++;
+            } else {
+                answer += str.charAt(i);
+                if(cnt > 1) {
+                    answer += String.valueOf(cnt);
+                    cnt =1;
+                }
+            }
+        }
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RepeatString T = new RepeatString();
+        Scanner sc = new Scanner(System.in);
+
+        String str= sc.next();
+
+        System.out.println(T.solution(str));
+
+
+    }
+
+}
